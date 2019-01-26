e2a6ca5255b552a1d501a27777b24ac6e0e512c3
Kay Jay <kharris2k16@gmail.com>
2019-01-26 09:31:42 -0800

    Prototypes Complete

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -27,7 +27,34 @@
 */
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+
+function CuboidMaker(attr) {
+  this.length = attr.length;
+  this.width = attr.width;
+  this.height = attr.height
+}
+CuboidMaker.prototype.volume = function(){
+  return `${this.length * this.width * this.height}`;
+}
+CuboidMaker.prototype.surfaceArea = function(){
+  const lw = this.length*this.width;
+  const lh = this.length*this.height;
+  const wh = this.width*this.height;
+  const area = lw + lh + wh  
+  return area*2;
+  // this seems like a clunky solution, id like to have found a cleaner way to do this
+}
+
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+});
+
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); //130
+
+
+
 
 
